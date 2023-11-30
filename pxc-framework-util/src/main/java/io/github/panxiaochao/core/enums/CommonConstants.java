677ad9dab867879f4a6dc14db10d35bc60237f0a
panxiaochao <panxiaochao602@qq.com>
2023-11-30 17:26:56 +0800

    fix(2.0.4-SNAPSHOT): :smile: [1.4]添加通用常量类

---
+++ pxc-framework-util/src/main/java/io/github/panxiaochao/core/enums/CommonConstants.java
@@ -0,0 +1,38 @@
+package io.github.panxiaochao.core.enums;
+
+/**
+ * <p>
+ * 通用类常量.
+ * </p>
+ *
+ * @author Lypxc
+ * @since 2023-11-30
+ */
+public interface CommonConstants {
+
+	/**
+	 * 通用成功标识
+	 */
+	Integer OK = 0;
+
+	/**
+	 * 通用失败标识
+	 */
+	Integer FAIL = 1;
+
+	/**
+	 * 是否为系统默认（是）
+	 */
+	String YES = "Y";
+
+	/**
+	 * 正常状态
+	 */
+	Integer STATUS_NORMAL = 1;
+
+	/**
+	 * 菜单根节点
+	 */
+	Long MENU_TREE_ROOT_ID = 0L;
+
+}
