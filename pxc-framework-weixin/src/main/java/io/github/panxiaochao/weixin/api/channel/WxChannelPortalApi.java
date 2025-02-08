0d0c3108a04018186cbbd61ec290b11bef011e74
panxiaochao <panxiaochao602@qq.com>
2025-02-08 11:26:53 +0800

    build[3.0.0-SNAPSHOT]: 更新 servlet-api 依赖至 Jakarta 版本
    [- 将 javax.servlet-api 替换为 jakarta.servlet-api
    - 更新相关 import 语句以使用 Jakarta命名空间
    - 此更改是为了适应 Java EE 向 Jakarta EE 的迁移]

--- pxc-framework-weixin/src/main/java/io/github/panxiaochao/weixin/api/channel/WxChannelPortalApi.java
+++ pxc-framework-weixin/src/main/java/io/github/panxiaochao/weixin/api/channel/WxChannelPortalApi.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
